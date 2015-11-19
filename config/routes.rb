0a2abbbbb4a99d655f6199ec44bec46a67a1a52b
Dennis Truong <dtruong02@gmail.com>
2015-11-19 13:25:01 -0500

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-	  devise_for :users
+
+  devise_for :users
 	  root to: 'teams#index'
 	  resources :teams do
 	    resources :players, only: [:create, :new, :update]
