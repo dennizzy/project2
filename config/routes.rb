3d143127ab9a53108cedf107d48059107911a52a
Mohamed Goodwin <mohamed.goodwin@gmail.com>
2015-11-19 16:45:50 -0500

    fix the changes to your routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 	  resources :teams do
 	    resources :players, only: [:create, :new, :update]
 	  end
-	  
+
 	  resources :players, only: [:index, :edit, :show, :destroy]
 end
+# explain the only: and what does it do?
+
+# why did you write resources:players twice?
+# make sure you omit edit and destroy in your second resource for players
