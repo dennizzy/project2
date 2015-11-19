54eb4376ecb2760f3cbf053cf8407bbe2c2e09b3
Dennis Truong <dtruong02@gmail.com>
2015-11-19 14:42:19 -0500

    devise actually works

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,3 @@ umcp = Team.create!(
 		school_name: "University of Maryland, College Park",
         photo_url: "http://s7d2.scene7.com/is/image/Fathead/lgo_ncaa_maryland_terrapins?layer=comp&fit=constrain&hei=300&wid=300&fmt=png-alpha&qlt=95,0&op_sharpen=1&resMode=bicub&op_usm=0.0,0.0,0,0&iccEmbed=0",
         state: "Maryland")
-
-dennis = Player.create!(
-		name: "Dennis Truong",
-		photo_url: "http://lh6.googleusercontent.com/-rpwI4wltXUM/AAAAAAAAAAI/AAAAAAAADXA/l96ETHD93vk/photo.jpg?sz=300",
-		position: "Safety",
-		year: "Alumni",
-		age: 25,
-		team: umcp
-	)
