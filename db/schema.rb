3fa7a2e9c503f5c3af274502759c6addf9013102
Dennis Truong <dtruong02@gmail.com>
2015-11-19 00:33:11 -0500

    routes

--- db/schema.rb
+++ db/schema.rb
@@ -4,16 +4,16 @@ ActiveRecord::Schema.define(version: 20151110183807) do
   enable_extension "plpgsql"
 
   create_table "teams", force: :cascade do |t|
-    t.string "school_name"
-    t.string "photo_url"
-    t.string "state"
+    t.string    "school_name"
+    t.string    "photo_url"
+    t.string    "state"
   end
 
   create_table "players", force: :cascade do |t|
-    t.string  "name"
-    t.string  "photo_url"
-    t.string  "position"
-    t.integer  "year"
-    t.integer "team_id"
+    t.string    "name"
+    t.string    "photo_url"
+    t.string    "position"
+    t.integer   "year"
+    t.integer   "team_id"
   end
 end
